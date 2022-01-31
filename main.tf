b23cf4e453953ae686976446ec0929a084c7e8b6
Shi Han NG <shihanng@gmail.com>
2022-01-31 19:56:00 +0900

    Add S3 backend

--- main.tf
+++ main.tf
@@ -1,5 +1,10 @@
 terraform {
   required_version = "~> 1.1.4"
+  backend "s3" {
+    bucket = "terraform-s3-backend-pmh86b2v"
+    region = "ap-northeast-1"
+    key    = "remote_terraform.tfstate"
+  }
 }
 
 provider "aws" {
